211d6c80bea4063b1755bcd9a20691c2e59e52a4
bhuvan2201 <bhuvan22100@gmail.com>
2023-03-29 14:31:36 -0500

    added

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var more=0;
 router.get('/', function(req, res, next) {
   more=more+1;
   sum=sum+more;
-  res.send('respond with a resource');
+  res.send('sum is:${sum}');
 });
 
 module.exports = router;
